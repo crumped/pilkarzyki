b2af949a648bf41b217eff04fd53bcf8afa4e81d
Kornel Pietrzyk <kppietrzyk@gmail.com>
2019-11-20 23:02:21 +0100

    create class Team file

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Pilkarzyki
+namespace pilkarzyki
 {
     public partial class Form1 : Form
     {
@@ -16,5 +16,6 @@ namespace Pilkarzyki
         {
             InitializeComponent();
         }
+
     }
 }
